01a6f50585ed2efac5242772b3ac0e8b0dc6ed22
Andreas Höhmann <andreas.hoehmann@gmx.de>
2012-02-08 15:14:48 +0100

    [add] play with spotlight [add] off/on lightning

--- src/test/java/de/bloxel/engine/jme/CubicMeshVolumeNodeTest.java
+++ src/test/java/de/bloxel/engine/jme/CubicMeshVolumeNodeTest.java
@@ -16,25 +16,29 @@
  *******************************************************************************/
 package de.bloxel.engine.jme;
 
-import java.util.Random;
-
 import com.jme3.app.SimpleApplication;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.Trigger;
 import com.jme3.light.AmbientLight;
+import com.jme3.light.SpotLight;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.post.FilterPostProcessor;
 import com.jme3.post.ssao.SSAOFilter;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
 import com.jme3.scene.Spatial;
+import com.jme3.shadow.PssmShadowRenderer;
+import com.jme3.shadow.PssmShadowRenderer.CompareMode;
+import com.jme3.shadow.PssmShadowRenderer.FilterMode;
 
 import de.bloxel.engine.data.Bloxel;
 import de.bloxel.engine.data.ColtVolumeFactory;
-import de.bloxel.engine.data.Volume;
 import de.bloxel.engine.data.VolumeGrid;
-import de.bloxel.engine.loader.Loader;
+import de.bloxel.engine.loader.RandomLoader;
 import de.bloxel.engine.material.ImageAtlasBloxelAssetManager;
 
 /**
@@ -43,30 +47,16 @@ import de.bloxel.engine.material.ImageAtlasBloxelAssetManager;
  */
 public class CubicMeshVolumeNodeTest extends SimpleApplication implements ActionListener {
 
-  static class DummyLoader implements Loader<Bloxel> {
-
-    @Override
-    public void fill(final Volume<Bloxel> aVolume) {
-      final Random r = new Random(19760901);
-      for (int x = 0; x < aVolume.getSizeX(); x++) {
-        for (int z = 0; z < aVolume.getSizeZ(); z++) {
-          for (int y = 0; y < aVolume.getSizeY(); y++) {
-            final Bloxel bloxel = new Bloxel(r.nextInt(6) + 1, r.nextFloat() / 10);
-            aVolume.set(x, y, z, r.nextBoolean() ? bloxel : Bloxel.AIR);
-          }
-        }
-      }
-    }
-  }
-
   public static void main(final String[] args) {
     new CubicMeshVolumeNodeTest().start();
   }
 
-  private boolean cubic = true;
   private boolean debug;
+  private boolean lightning;
   private VolumeGrid<Bloxel> grid;
   private ImageAtlasBloxelAssetManager bloxelAssetManager;
+  private SpotLight spot;
+  private Vector2f screenCenter;
 
   private void addMapping(final String name, final Trigger trigger) {
     getInputManager().addMapping(name, trigger);
@@ -75,65 +65,87 @@ public class CubicMeshVolumeNodeTest extends SimpleApplication implements Action
 
   private void fill(final VolumeGrid<Bloxel> grid) {
     rootNode.detachAllChildren();
-    for (int z = -1; z < 1; z++) {
-      for (int y = -1; y < 1; y++) {
-        rootNode.attachChild(node(grid, 0, y, z));
-      }
-    }
+    rootNode.attachChild(node(grid, 0, 0, 0));
   }
 
   private AbstractVolumeNode node(final VolumeGrid<Bloxel> grid, final int x, final int y, final int z) {
-    final AbstractVolumeNode volumeNode = cubic ? new CubicMeshVolumeNode(grid, grid.getVolume(x, y, z),
-        bloxelAssetManager) : new SmoothSurfaceVolumeNode(grid, grid.getVolume(x, y, z), bloxelAssetManager);
+    final AbstractVolumeNode volumeNode = new CubicMeshVolumeNode(grid, grid.getVolume(x, y, z), assetManager,
+        bloxelAssetManager);
     volumeNode.calculate();
     volumeNode.update();
     volumeNode.debug(debug);
+    volumeNode.setShadowMode(ShadowMode.Cast);
     return volumeNode;
   }
 
   @Override
   public void onAction(final String name, final boolean isPressed, final float tpf) {
-    if (name.equals("cubic") && isPressed) {
-      cubic = true;
-      fill(grid);
-    }
-    if (name.equals("smooth") && isPressed) {
-      cubic = false;
-      fill(grid);
-    }
     if (name.equals("debug") && isPressed) {
       debug = !debug;
       for (final Spatial n : rootNode.getChildren()) {
         ((AbstractVolumeNode) n).debug(debug);
       }
     }
+    if (name.equals("lightning") && isPressed) {
+      lightning = !lightning;
+      bloxelAssetManager.setLightning(lightning);
+      fill(grid);
+    }
+  }
+
+  public void setupLighting() {
+    final AmbientLight al = new AmbientLight();
+    al.setColor(ColorRGBA.White.mult(0.8f));
+    rootNode.addLight(al);
+    spot = new SpotLight();
+    spot.setSpotRange(50);
+    spot.setSpotInnerAngle(5 * FastMath.DEG_TO_RAD);
+    spot.setSpotOuterAngle(20 * FastMath.DEG_TO_RAD);
+    spot.setPosition(cam.getLocation());
+    spot.setDirection(cam.getWorldCoordinates(screenCenter, 0).subtract(spot.getPosition()));
+    spot.setColor(ColorRGBA.White.mult(2));
+    rootNode.addLight(spot);
   }
 
   @Override
   public void simpleInitApp() {
     bloxelAssetManager = new ImageAtlasBloxelAssetManager(assetManager);
+    screenCenter = new Vector2f(settings.getWidth() / 2, settings.getHeight() / 2);
     grid = new VolumeGrid<Bloxel>();
     grid.setGridSize(16, 4, 16);
     grid.setVolumeSize(16);
     // grid.setVolumeLoader(new BlockmaniaTerrainLoader("cPpCzKqBpVkQpVjP"));
-    grid.setVolumeLoader(new DummyLoader());
+    grid.setVolumeLoader(new RandomLoader());
     grid.setVolumeFactory(new ColtVolumeFactory<Bloxel>());
     grid.init();
     fill(grid);
-    cam.setLocation(Vector3f.ZERO);
-    cam.setFrustumFar(3000f);
+    cam.setLocation(Vector3f.ZERO.add(0, 0, 50));
+    cam.setFrustumFar(1000f);
     flyCam.setMoveSpeed(50);
     flyCam.setEnabled(true);
-    final AmbientLight l = new AmbientLight();
-    l.setColor(ColorRGBA.White.mult(3));
-    rootNode.addLight(l);
     final FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
     final SSAOFilter ssaoFilter = new SSAOFilter();
     ssaoFilter.setEnabled(false);
     fpp.addFilter(ssaoFilter);
     viewPort.addProcessor(fpp);
-    addMapping("cubic", new KeyTrigger(KeyInput.KEY_1));
+    final PssmShadowRenderer pssmRenderer = new PssmShadowRenderer(assetManager, 1024, 3);
+    pssmRenderer.setDirection(new Vector3f(-1, -1, -1).normalizeLocal());
+    pssmRenderer.setLambda(0.55f);
+    pssmRenderer.setShadowIntensity(0.6f);
+    pssmRenderer.setCompareMode(CompareMode.Software);
+    pssmRenderer.setFilterMode(FilterMode.Bilinear);
+    pssmRenderer.displayDebug();
+    viewPort.addProcessor(pssmRenderer);
+    addMapping("lightning", new KeyTrigger(KeyInput.KEY_L));
     addMapping("smooth", new KeyTrigger(KeyInput.KEY_2));
     addMapping("debug", new KeyTrigger(KeyInput.KEY_SPACE));
+    setupLighting();
+  }
+
+  @Override
+  public void simpleUpdate(final float tpf) {
+    super.simpleUpdate(tpf);
+    spot.setPosition(cam.getLocation());
+    spot.setDirection(cam.getWorldCoordinates(screenCenter, 0).subtract(spot.getPosition()));
   }
 }
